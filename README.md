4e3c22bef09de5fdd846deaa7b65ab7b6ac3c283
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2024-09-15 20:18:30 -0300

    First Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Jogo-do-C
+Status: Em desenvolvimento
+
+Jogo de perguntas e resposta com rank de pontos.
---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int point,total;
+
+    printf("Bem vindo ao Jogo do C \n");
+    printf("Voce deseja comecar a jogar ? \n \n Sim ou Nao");
+    scanf("%d",&i);
+}
