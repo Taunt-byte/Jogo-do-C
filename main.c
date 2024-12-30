dd4f98fb3cabf9cb3127ddeff3ec40c2f4e744df
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2024-12-30 15:29:38 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
+// Data: 30/12/24
+// Autor: Lucas Neves da Silva
+
 #include <stdio.h>
 
 int main(){
-    printf("Bem vindo ao Jogo do C \n");
+    int start, pontos , placar;
+    printf("Bem vindo ao Terminus\n seu jogo de perguntas e resposta \n feita na linguagem C por Lucas Neves");
     printf("Voce deseja comecar a jogar ? \n \n Sim ou Nao");
+    scanf(&start);
+    if (start == 1){
+        printf("O jogo ira começar");
+        break
+    }else(start == 2){
+        printf("O jogo acabou");
+    }
 }
