e21393763cf49f43d4fc8b34111bc1f48366b500
Lucas Neves <71947586+Taunt-byte@users.noreply.github.com>
2024-12-30 15:14:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,9 +1,6 @@
 #include <stdio.h>
 
 int main(){
-    int point,total;
-
     printf("Bem vindo ao Jogo do C \n");
     printf("Voce deseja comecar a jogar ? \n \n Sim ou Nao");
-    scanf("%d",&i);
 }
